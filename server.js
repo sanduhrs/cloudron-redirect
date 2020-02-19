c97439129256f3d9af5f4499ed6c36dd667bedcc
Stefan Auditor <stefan.auditor@erdfisch.de>
2020-02-19 18:19:43 +0100

    prevent white flickr

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var server = http.createServer(function (request, response) {
+  if (process.env.TIMEOUT == 0) {
+    response.writeHead(302, {"Location": process.env.URL});
+  }
   response.write(`<html><head><meta http-equiv="refresh" content="${process.env.TIMEOUT}; URL='${process.env.URL}'" /><body>${process.env.MESSAGE}`);
   response.end();
 });
